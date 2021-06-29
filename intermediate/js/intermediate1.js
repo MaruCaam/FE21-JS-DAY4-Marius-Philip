783884f114a4e80e842e09ac6015556e40ab3505
philipm92 <philip.mahlberg@hotmail.com>
2021-06-29 12:00:25 +0200

    make santa appear and disappear

--- intermediate/js/intermediate1.js
+++ intermediate/js/intermediate1.js
@@ -1,10 +1,17 @@
-function ByeByeSanta() {
-    this.style.opacity= "0";
-    this.style.transition = "1000ms";    
-}
+function ByeHelloSanta() {
+    if (times_clicked%2 == 0) {
+        this.style.opacity= "0";
+        this.style.transition = "1000ms";    
+    } else {
+        this.style.opacity= "1";
+        this.style.transition = "250ms";
+    }
+    times_clicked++;
+}    
 
 // 2,5,7,9
 var santa_items = [2,5,7,9];
 for (let santa of santa_items) {
-    document.getElementsByClassName(`item${santa}`)[0].addEventListener("click", ByeByeSanta);
+    var times_clicked = 0;
+    document.getElementsByClassName(`item${santa}`)[0].addEventListener("click", ByeHelloSanta);
 }
