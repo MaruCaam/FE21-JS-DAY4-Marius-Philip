d28bdc5cf9730ec08efc084fd7db4c4721e86f0c
philipm92 <philip.mahlberg@hotmail.com>
2021-06-29 13:37:12 +0200

    added colors to basic

--- basic/js/basic1.js
+++ basic/js/basic1.js
@@ -12,17 +12,34 @@
 
 
 function DistributeData() {
-    let firstname = document.forms["personaldata"]["firstname"].value;
-    let lastname = document.forms["personaldata"]["lastname"].value;
-    let age = document.forms["personaldata"]["age"].value;
-    // alert(firstname + " " + lastname + " " + age);
-    document.getElementById("fname").innerHTML += firstname;
-    document.getElementById("lname").innerHTML += lastname;
-    document.getElementById("age").innerHTML += age;
+    // let firstname = document.forms["personaldata"]["firstname"].value;
+    // let lastname = document.forms["personaldata"]["lastname"].value;
+    // let age = document.forms["personaldata"]["age"].value;
+    let form_firstname = document.getElementsByName("firstname")[0].value;
+    let form_lastname = document.getElementsByName("lastname")[0].value;
+    let form_age = document.getElementsByName("age")[0].value;
+
+    let div_firstname = document.getElementById("fname");
+    let div_lastname = document.getElementById("lname");
+    let div_age = document.getElementById("age");
+
+    // add values to right div
+    div_firstname.innerHTML = form_firstname;
+    div_lastname.innerHTML = form_lastname;
+    div_age.innerHTML = form_age;
+
+    // Play with Colors
+    div_firstname.style.color = "red";
+    if (form_firstname.length > 5) div_firstname.style.color = "green";
+    
+    div_lastname.style.color = "red"; 
+    if (form_lastname.length > 5) div_lastname.style.color = "green";
+
+    // event.preventDefault();
 
 }
 
-document.getElementById("btn").addEventListener("click", DistributeData, false);
+document.getElementById("btn").addEventListener("click", DistributeData);
 
 
 
